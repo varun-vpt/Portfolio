8a84b2a21e49129ea1177abac1aae5bd1e08b426
VARUN <varun@VAUN-MacBook-Air.local>
2024-07-17 17:33:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,251 @@
+
+
+// ------------------Tells if a number is positive or negetive------------------
+// let a = 0
+
+// if(a >= 0){
+//     console.log("Number is positive")
+// }else{
+//     console.log("Number is negetive")
+// }
+
+
+// ------------------Tells if a number is Even or Odd------------------
+// let a = 4378
+
+// if(a % 2 == 0){
+//     console.log("Number is Even")
+// }else{
+//     console.log("Number is Odd")
+// }
+
+
+// ------------------Tells if a number is Greater than other------------------
+
+// a = 5
+// b = 6
+
+// if(a < b){
+//     console.log("b is greater than a")
+// }else if(a > b){
+//     console.log("a is greater than b")
+// }else{
+//     console.log("Numbers are same")
+// }
+
+// ------------------Tells if a number is Greater than other between 3 number------------------
+// a = 10
+// b = 20
+// c = 30
+
+// if(a > b){
+//     if(a > c){
+//         console.log("A is greater")
+//     }else{
+//         console.log("C is greater")
+//     }
+// }else if(a < b){
+//     if(b > c){
+//         console.log("B is greater")
+//     }else{
+//         console.log("C is greater")
+//     }
+// }
+
+// if(a > b && a > c){
+//     console.log("A is greater")
+// }else if(b > a && b > c){
+//     console.log("B is greater")
+// }else{
+//     console.log("C is greater")
+// }
+
+// ------------------Grade To Alphabet------------------
+// 10 - A+
+// 9 - A
+// 8 - B+
+// 7 - B
+// 6 - C+
+// 5 - C
+// 4 - D+
+// 3 - D
+// 2 - F+
+// 1 - F
+
+
+// let grade = 6
+
+// if ( grade == 8){
+//     console.log("B+")
+// }
+// else if (grade == 1){
+//     console.log("F")
+// }
+// else if (grade == 2){
+//     console.log("F+")
+// }
+// else if (grade == 3){
+//     console.log("D")
+// }
+// else if (grade == 4){
+//     console.log("D+")
+// }
+// else if (grade == 5){
+//     console.log("C")
+// }
+// else if (grade == 6){
+//     console.log("c+")
+// }
+// else if (grade == 7){
+//     console.log("B")
+// }
+// else if (grade == 9){
+//     console.log("A")
+// }
+// else if (grade == 10){
+//     console.log("A+")
+// }
+
+
+// a = 50
+// number = 30
+// if (number => a){
+//     console.log("higher")
+// }
+// else if (number < a) {
+//     console.log("smaller")
+// }
+
+// let randomNUmber = Math.floor( Math.random()  *100);
+// console.log(randomNUmber)
+
+
+// let guess = 37 
+// if (guess => a){
+    
+
+// }
+
+
+
+
+// let productPrice = 1200
+
+// if (productPrice >= 100){
+//     console.log(`Your final price is - ${productPrice - 20}`)
+// }
+
+// else if (productPrice >= 50){
+//     console.log(`Your final price is - ${productPrice - 10}`)
+// }
+
+// else if (productPrice < 50){
+//     console.log(`Your final price is - ${productPrice}`)
+// }
+
+
+// 3.	Check if a number is greater than 100.
+
+
+// let number = 100
+
+// if (number > 100){
+//     console.log("greaterthan 100")
+// }
+
+// else if (number == 100){
+//     console.log("equal to 100")
+// }
+
+// else if (number <100){
+//     console.log("smaller than 100")
+// }
+
+
+// 7.	Check if a person’s age is over 18.
+
+// let personsAge = 20
+
+// if (personsAge > 18 ) {
+//     console.log("Person's age is over 18")
+// }
+
+// else if (personsAge == 18) {
+//     console.log("Person's age is 18")
+// }
+
+// else if (personsAge < 18) {
+//     console.log("Person's age is less than 18")
+// }
+
+
+// 4.	Check if a number is divisible by 5.
+
+// let number = 30
+
+// if (number % 5 == 0) {
+//   console.log("The number is divisible by 5");
+// } 
+
+// else {
+//   console.log("The number is not divisible by 5");
+// }
+
+
+// let num = 10  
+// let sum = 0
+// for (let value = 1; value <=num; value++){
+//      sum = sum+value
+//     }\
+//     console.log(sum)
+
+
+// let a = 2 
+// let b = 3
+
+
+// console.log({a,b}) 
+
+
+// a = a*b
+// b = a/b 
+// a = a/b
+
+
+// console.log({a,b}) 
+
+// let number = 153;
+// let lastDigit;
+// let sum = 0;
+// let copy = number
+
+// while (number > 0) {
+//     lastDigit = number % 10; 
+//     number = Math.floor(number / 10);
+//      sum = sum + lastDigit **3;
+    
+// }
+
+// console.log(copy,sum)
+
+
+
+// let a = "hello";
+// let b;
+
+
+
+// for ( let i=0 ;i<0; i++) {
+//   console.log(i)   
+// }
+
+if(true){
+ console.log("hello");
+}
+
+
+// var i = 0;
+// do{
+//     i++;
+//     console.log(i);
+// } while(false)
